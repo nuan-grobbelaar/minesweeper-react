d10a2aea9979528ab85e6e8ee123e695592dcc20
nuan <nuangrobbelaar@icloud.com>
2025-05-27 07:34:48 +0200

    buttons

---
+++ src/Button.tsx
@@ -0,0 +1,31 @@
+const Button = (props) => {
+	console.log(props.selected);
+	return (
+		<div
+			id={props.id}
+			className={`brutalButton ${props.className} ${
+				props.selected ? "selected" : ""
+			}`}
+		>
+			<div className="brutalButton__container">
+				{!props.selected && <div className="brutalButton__shadow" />}
+				<button
+					className={props.className + " brutalButton__container__button"}
+					style={
+						!props.disabled
+							? { backgroundColor: props.color }
+							: { backgroundColor: "#bbb" }
+					}
+					onClick={props.onClick}
+					disabled={props.disabled}
+				>
+					<div className={"brutalButton__container__button__text"}>
+						{props.children}
+					</div>
+				</button>
+			</div>
+		</div>
+	);
+};
+
+export default Button;
--- src/GameEmbed.tsx
+++ src/GameEmbed.tsx
@@ -1,16 +1,41 @@
-import React from "react";
+import { useRef, useState } from "react";
+import Button from "./Button";
 
 const GameEmbed = () => {
+	const iframeRef = useRef(null);
+	const [defuseMode, setDefuseMode] = useState(false);
+
+	function toggleDefuseMode() {
+		setDefuseMode(!defuseMode);
+		document
+			.getElementById("game")
+			?.contentWindow?.callGodotToggleDefuseMode(true);
+	}
+
 	return (
-		<div style={{ width: "100%", height: "80vh" }}>
-			<iframe
-				title="Godot Game"
-				src="/godot/Minesweeper.html"
-				width="100%"
-				height="100%"
-				allowFullScreen
-			/>
-		</div>
+		<>
+			<div className="game-container">
+				<iframe
+					id="game"
+					ref={iframeRef}
+					title="Godot Game"
+					src="/godot/Minesweeper.html"
+					width="100%"
+					height="100%"
+					allowFullScreen
+				/>
+			</div>
+			<div className="action-bar">
+				<Button
+					className="action-button"
+					onClick={() => toggleDefuseMode()}
+					color="#ffffff"
+					selected={defuseMode}
+				>
+					Click
+				</Button>
+			</div>
+		</>
 	);
 };
 
