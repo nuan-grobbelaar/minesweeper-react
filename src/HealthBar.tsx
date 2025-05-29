17eb5e193e498ebf64bd50f7e9a4ebcf9ef6c1a5
nuan <nuangrobbelaar@icloud.com>
2025-05-29 19:03:37 +0200

    state

--- src/HealthBar.tsx
+++ src/HealthBar.tsx
@@ -2,7 +2,7 @@ const HealthBar = (props: any) => {
 	return (
 		<div
 			className={`health-bar ${
-				props.value == props.max || props.value < 0 ? "end" : ""
+				props.value == props.max || props.value <= 0 ? "end" : ""
 			}`}
 		>
 			<progress max={props.max} value={props.value}></progress>
